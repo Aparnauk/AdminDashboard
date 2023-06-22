4e66091eb627454809307b6c6ec6712a69fbbb56
Aparnauk <aparnauk22@gmail.com>
2023-06-22 10:48:01 +0530

    admindashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { LeadComponent } from './lead/lead.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'dashboard', component:DashboardComponent},
+  {path:'', redirectTo:'dashboard', pathMatch:'full'},
+  {path:'employees',component:EmployeesComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'lead',component:LeadComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { }                    
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'admindashboard';
+  title = 'admin-dashboard';
+  sidebarOpene:boolean=true
+  toggle(){
+    this.sidebarOpene=!this.sidebarOpene
+  }
 }
+
